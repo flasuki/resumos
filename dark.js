feafe26a9ca803d6e0ca84a4565556c7969d1a9b
flasuki <luismiguelfernansilva@gmail.com>
2025-05-18 20:55:51 -0300

    botao do dark mode mais bonito

--- dark.js
+++ dark.js
@@ -1,11 +1,21 @@
 const btn = document.getElementById('toggle-theme');
+const icon = btn.querySelector('i');
+
+if(localStorage.getItem('dark-mode') === 'enabled'){
+  document.body.classList.add('dark-theme');
+  icon.className = 'fas fa-sun';
+} else {
+  icon.className = 'fas fa-moon';
+}
 
 btn.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
-  
+
   if(document.body.classList.contains('dark-theme')){
-    btn.textContent = 'Light Mode';
+    localStorage.setItem('dark-mode', 'enabled');
+    icon.className = 'fas fa-sun';
   } else {
-    btn.textContent = 'Dark Mode';
+    localStorage.setItem('dark-mode', 'disabled');
+    icon.className = 'fas fa-moon';
   }
 });
